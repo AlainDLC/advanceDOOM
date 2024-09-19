1b5c9b81fd8d344bedb8f33db42c2cfb4496c2e9
AlainDLC <jd_alain@hotmail.com>
2024-09-19 21:04:02 +0200

    playing with the doom

--- doom/script.js
+++ doom/script.js
@@ -30,7 +30,87 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-//
+/////////////////////////////////////////////////////
+//PAGA NAV //
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/*
+
+document.querySelectorAll('.nav__link').forEach(el => {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    console.log(id);
+  });
+});
+*/
+
+// 1. Add event listner to common parant element
+// 2. Determine what elemet originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Mathing strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id)?.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+///////////////////////////
+///////////////////////////
+// styles
+
+/*
+message.style.backgroundColor = '#706c5f';
+message.style.borderRadius = '8px';
+message.style.color = '#FFFFFF';
+message.style.width = '120%';
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 10 + 'px';
+
+const logo = document.querySelector('.nav__logo');
+
+const link = document.querySelector('.twitter-link');
+*/
+/*
+const h1 = document.querySelector('h1');
+
+const alertH1 = () => {
+  alert('mouse');
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+*/
+/*
+const randomInts = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randomColor = () =>
+  `rgb(${randomInts(0, 255)},${randomInts(0, 255)},${randomInts(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector('.nav__links').addEventListener('click', function () {
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav').addEventListener('click', function () {
+  this.style.backgroundColor = randomColor();
+});
+
 
 const header = document.querySelector('header');
 
@@ -49,54 +129,4 @@ header.append(message);
 document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   message.remove();
 });
-
-// styles
-
-message.style.backgroundColor = '#706c5f';
-message.style.borderRadius = '8px';
-message.style.color = '#FFFFFF';
-message.style.width = '120%';
-
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 10 + 'px';
-
-const logo = document.querySelector('.nav__logo');
-
-const link = document.querySelector('.twitter-link');
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click', e => {
-  /*const s1corords = section1.getBoundingClientRect();
-   console.log(s1corords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  console.log('Current scroll (X/Y)', window.scrollX, window.scrollY);
 */
-  // Scrolling kalkoreraing för hela scrolen
-
-  /* window.scrollTo(
-    s1corords.left + window.screenX,
-    s1corords.top + window.scrollY
-  );
-
-  window.scrollTo({
-    left: s1corords.left + window.screenX,
-    top: s1corords.top + window.scrollY,
-    behavior: 'smooth',
-  });
-  */
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-
-const h1 = document.querySelector('h1');
-
-const alertH1 = () => {
-  alert('mouse');
-  h1.removeEventListener('mouseenter', alertH1);
-};
-
-h1.addEventListener('mouseenter', alertH1);
