aa52eacddbb52f585ff3c747b34682b46b26de9f
AlainDLC <jd_alain@hotmail.com>
2024-09-23 14:05:13 +0200

    handlehover

--- doom/script.js
+++ doom/script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContanier = document.querySelector('.operations__tab-container');
+const tabContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -67,10 +71,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 // tabbar operations
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContanier = document.querySelector('.operations__tab-container');
-const tabContent = document.querySelectorAll('.operations__content');
-
 tabsContanier.addEventListener('click', e => {
   const clicked = e.target.closest('.operations__tab');
 
@@ -89,4 +89,26 @@ tabsContanier.addEventListener('click', e => {
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
+});
+
+// animation
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', e => {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener('mouseout', e => {
+  handleHover(e, 1);
 });
